52837cc76948c8cc09635cc524194799fde3aad1
PrayogaAjityaSetiawan <ajityaprayoga123@gmail.com>
2026-02-04 09:24:46 +0700

    upgrade version

--- src/app/alquran/[id]/page.jsx
+++ src/app/alquran/[id]/page.jsx
@@ -1,8 +1,8 @@
 import AyatListClient from "@/components/ayatListClient";
 
 export default async function Page({ params }) {
-  const { id } = params;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE}/v2/surat/${id}`, { cache: "no-store" });
+  const { id } = await params;
+  const res = await fetch(`https://equran.id/api/v2/surat/${id}`);
   const data = await res.json();
 
   if (!data.data?.ayat) return <p>Data tidak ditemukan</p>;
