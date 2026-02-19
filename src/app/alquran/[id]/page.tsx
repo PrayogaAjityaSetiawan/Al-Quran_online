513bf1cc2045486745127635337e5a82533b3692
PrayogaAjityaSetiawan <ajityaprayoga123@gmail.com>
2026-02-19 15:03:03 +0700

    fix error

--- src/app/alquran/[id]/page.tsx
+++ src/app/alquran/[id]/page.tsx
@@ -9,18 +9,7 @@ export default async function Page({ params }) {
 
   return (
     <div className="min-h-screen w-full px-4">
-      <AyatListClient ayatList={data.data.ayat} suratInfo={
-        {
-          nomor: data.data.nomor,
-          nama: data.data.nama,
-          namaLatin: data.data.namaLatin,
-          jumlahAyat: data.data.jumlahAyat,
-          tempatTurun: data.data.tempatTurun,
-          arti: data.data.arti,
-          deskripsi: data.data.deskripsi,
-          audioFull: data.data.audioFull,
-        }
-      } />
+      <AyatListClient ayatList={data.data.ayat} />
     </div>
   );
 }
